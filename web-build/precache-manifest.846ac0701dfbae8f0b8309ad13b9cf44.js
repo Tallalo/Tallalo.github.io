a79af7db0e6d994f8a1f3cefc56260e7cfb26a03
Tamar Menteshashvili <tamarimenteshashvili7@gmail.com>
2020-05-01 20:01:03 +0400

    movies

---
+++ web-build/precache-manifest.846ac0701dfbae8f0b8309ad13b9cf44.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2237ff67b24a499cb154",
+    "url": "/static/js/app.05f11ffb.chunk.js"
+  },
+  {
+    "revision": "dc6d7c46a725d6866bcd",
+    "url": "/static/js/runtime~app.44ddfa1e.js"
+  },
+  {
+    "revision": "9aa29e640557b71bbfae",
+    "url": "/static/js/2.184d7edf.chunk.js"
+  },
+  {
+    "revision": "0409a43627f2a6f82101ecdfaad6ee7c",
+    "url": "/static/media/eye-black.0409a436.png"
+  },
+  {
+    "revision": "d2fabd350bec3e1057f589fcd758b4c4",
+    "url": "/static/media/tickets-black.d2fabd35.png"
+  },
+  {
+    "revision": "9598c2a5664cef0bf9e47a668697fc32",
+    "url": "/static/media/phone-black.9598c2a5.png"
+  },
+  {
+    "revision": "3e67b06dd8d0f804220ae7317ce181ed",
+    "url": "/static/media/plus-black.3e67b06d.png"
+  },
+  {
+    "revision": "f1635b30e0523684d7e656b183b80e1f",
+    "url": "/static/media/tvs-black.f1635b30.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "76d4b1d09ff10b223209abf8e165fc87",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "6506a7f00a26e846a4679115afc7e33f",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "4829882f817fd113a984d58ad7c150c2",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "0cd29a3747a8c4a80bfe03d4f06d1d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f03c618e2f20038299e21029e134a2e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
