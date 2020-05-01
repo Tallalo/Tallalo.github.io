0cfb7c6a2484ec96e8b60af6115647ad76002817
Tamar Menteshashvili <tamarimenteshashvili7@gmail.com>
2020-05-01 21:32:50 +0400

    commmmiiiit

---
+++ precache-manifest.d74388923fcbadf074b48278c151520f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "6342e03cbed441fbc4c6",
+    "url": "/static/js/app.af72eb4d.chunk.js"
+  },
+  {
+    "revision": "dc6d7c46a725d6866bcd",
+    "url": "/static/js/runtime~app.44ddfa1e.js"
+  },
+  {
+    "revision": "895343031a393fdd824c",
+    "url": "/static/js/2.84b99e31.chunk.js"
+  },
+  {
+    "revision": "34a211ea55030344967d42b535df99f2",
+    "url": "/static/media/single-corn.34a211ea.png"
+  },
+  {
+    "revision": "0409a43627f2a6f82101ecdfaad6ee7c",
+    "url": "/static/media/eye-black.0409a436.png"
+  },
+  {
+    "revision": "d2fabd350bec3e1057f589fcd758b4c4",
+    "url": "/static/media/tickets-black.d2fabd35.png"
+  },
+  {
+    "revision": "9598c2a5664cef0bf9e47a668697fc32",
+    "url": "/static/media/phone-black.9598c2a5.png"
+  },
+  {
+    "revision": "3e67b06dd8d0f804220ae7317ce181ed",
+    "url": "/static/media/plus-black.3e67b06d.png"
+  },
+  {
+    "revision": "f1635b30e0523684d7e656b183b80e1f",
+    "url": "/static/media/tvs-black.f1635b30.png"
+  },
+  {
+    "revision": "957813663f94de64566872976a4f328c",
+    "url": "/static/media/popcorn-main-bunch.95781366.png"
+  },
+  {
+    "revision": "1af9874e8fe7ff074d8bad7cc30ae45c",
+    "url": "/static/media/popcorn-box.1af9874e.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "76d4b1d09ff10b223209abf8e165fc87",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "6506a7f00a26e846a4679115afc7e33f",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "4829882f817fd113a984d58ad7c150c2",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "30ab380635a142c3796516f2d9b0153c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf97d096e3cf9d005611152b7dd703f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
